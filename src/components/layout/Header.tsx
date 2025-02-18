7fcc64ec7f0dcaf9985a741b54740e24e15ac019
Byron Keet <keetbis@gmail.com>
2025-02-18 12:13:09 +0200

    Add logo home page link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,16 @@
 import { LanguagePicker } from "../LanguagePicker";
+import Link from "next/link";
 
 export function Header() {
 	return (
 		<header className='w-full p-4'>
 			<div className='flex justify-between items-center'>
-				<h1 className='text-2xl font-serif'>TULUDI</h1>
+				<Link
+					href='/'
+					className='text-2xl font-serif hover:opacity-80'
+				>
+					TULUDI
+				</Link>
 				<LanguagePicker />
 			</div>
 		</header>
